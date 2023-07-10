92c727b3db2fe0dd3d1e0e4833189758dcb1fb15
sgbbco <116119784+sgbbco@users.noreply.github.com>
2023-07-10 15:21:57 -0600

    All game logic and features are fixed. Design and Styling completed.

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,3 +1,13 @@
+// Map playerScoreboard, cpuScoreboard to update id='scoreboard'
+const playerScoreboard = document.getElementById('playerScore');
+const cpuScoreboard = document.getElementById('cpuScore');
+
+// Map choices to image URLs
+const imageUrls = {
+  'rock': 'https://www.nicepng.com/png/detail/6-61708_rock-rock-paper-scissors-clipart.png',
+  'paper': 'https://www.pngitem.com/pimgs/m/266-2667252_transparent-rock-paper-scissors-clipart-rock-paper-scissors.png',
+  'scissors': 'https://www.seekpng.com/png/detail/111-1114370_rock-paper-scissors-rock-paper-scissors-clipart.png'
+};
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
@@ -19,6 +29,25 @@ function getComputerSelection() {
   return choices[randomIndex];
 }
 
+function displayBattlefield(playerSelection, computerSelection) {
+  var playerDiv = document.getElementById('playerChoice');
+  var computerDiv = document.getElementById('computerChoice');
+
+  // Remove any existing images
+  while (playerDiv.firstChild) playerDiv.removeChild(playerDiv.firstChild);
+  while (computerDiv.firstChild) computerDiv.removeChild(computerDiv.firstChild);
+
+  // Create new images and add them to the battlefield divs
+  var playerImg = document.createElement('img');
+  var computerImg = document.createElement('img');
+  playerImg.src = imageUrls[playerSelection]; // look up image URL based on player's choice
+  computerImg.src = imageUrls[computerSelection]; // look up image URL based on computer's choice
+  playerImg.alt = playerSelection;
+  computerImg.alt = computerSelection;
+  playerDiv.append(playerImg);
+  computerDiv.append(computerImg);
+}
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -31,6 +60,8 @@ buttons.forEach((button) => {
     const computerSelection = getComputerSelection();
     const result = playRound(playerSelection, computerSelection);
 
+    displayBattlefield(playerSelection, computerSelection);
+
     if (result && typeof result === 'string') {
       if (result.includes('Win')) {
         playerScore++;
@@ -39,7 +70,9 @@ buttons.forEach((button) => {
       }
     }
 
-    resultsDiv.textContent = result + ' Score: ' + playerScore + ' - ' + computerScore;
+    resultsDiv.textContent = result;
+    playerScoreboard.textContent = playerScore;
+    cpuScoreboard.textContent = computerScore;
 
     if (playerScore == 5 || computerScore == 5) {
       resultsDiv.textContent += ' Game Over. ' + (playerScore > computerScore ? 'You are the winner!' : 'Computer is the winner!');
